99e788fc3d30aa75ecef7e1fb9da39a0eba0e90f
156357 <b.bochomulski@gmail.com>
2022-04-27 00:32:48 +0200

    Lab_08

--- qr_matrix.py
+++ qr_matrix.py
@@ -2,17 +2,14 @@ import numpy as np
 
 
 A = np.array([
-    [1, 0, 1],
-    [1, 1, 0],
-    [0, 1, 0],
-    [1, 0, 1],
+    [2, 4, 1, 6],
+    [8, 1, 2, 3],
+    [1, 2, 1, 5],
+    [3, 1, 2, 7],
 ])
 
 
 def qr_matrix(matrix):
-    """
-    Return the QR decomposition of a matrix
-    """
     list_of_vectors = []
     for i in range(matrix.shape[1]):
         vector = np.array([[matrix[j][i]] for j in range(matrix.shape[0])])
@@ -35,9 +32,22 @@ def qr_matrix(matrix):
     return Q, R
 
 
-Q, R = qr_matrix(A)
+def matrix_eigenvalues(matrix):
+    if matrix.shape[0] != matrix.shape[1]:
+        return "Matrix is not square"
+    print("Expected outcome: \n", np.linalg.eigvals(matrix))
+    matrix = matrix.copy()
+    eigenvalues = np.diag(matrix)
+    eigenvalues_new = np.ones(matrix.shape[0])
+    i = 0
+    while not np.allclose(eigenvalues, eigenvalues_new, 0.000000001):
+        eigenvalues = eigenvalues_new
+        Q, R = qr_matrix(matrix)
+        matrix = np.dot(R, Q)
+        eigenvalues_new = np.diag(matrix)
+        i += 1
+    print("Iterations: ", i)
+    return eigenvalues_new
 
-print(f"A:\n{A}")
-print(f"Q:\n{np.round(Q, 4)}")
-print(f"R:\n{np.round(R, 4)}")
-print(f"QR:\n{np.dot(Q, R).astype(int)}")
+
+print("Outcome: \n", matrix_eigenvalues(A))
