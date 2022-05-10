57fcb22059dc1cccb27c39430b1e15694759e24a
Bartosz Bochomulski <b.bochomulski@gmail.com>
2022-05-11 01:57:15 +0200

    Zrobione ale nie dziala

--- qr_matrix.py
+++ qr_matrix.py
@@ -1,11 +1,15 @@
 import numpy as np
 
-
 A = np.array([
     [2, 4, 1, 6],
     [8, 1, 2, 3],
     [1, 2, 1, 5],
-    [3, 1, 2, 7],
+    [3, 1, 2, 7]
+])
+A = np.array([
+    [2, 2, 1],
+    [6, 4, 0],
+    [2, 2, 2]
 ])
 
 
@@ -24,7 +28,8 @@ def qr_matrix(matrix):
             projections_sum += (np.dot(u_vector.T, vector)/np.dot(u_vector.T, u_vector))*u_vector
         u_vector = vector - projections_sum
         list_of_u_vectors.append(u_vector)
-        list_of_e_vectors.append(u_vector/(np.sum(u_vector**2)**0.5))
+        e_vector = u_vector/(np.sum(u_vector**2)**0.5)
+        list_of_e_vectors.append(e_vector)
 
     Q = np.array(list_of_e_vectors).T[0]
     R = np.dot(Q.T, matrix)
@@ -35,19 +40,54 @@ def qr_matrix(matrix):
 def matrix_eigenvalues(matrix):
     if matrix.shape[0] != matrix.shape[1]:
         return "Matrix is not square"
-    print("Expected outcome: \n", np.linalg.eigvals(matrix))
     matrix = matrix.copy()
     eigenvalues = np.diag(matrix)
     eigenvalues_new = np.ones(matrix.shape[0])
-    i = 0
-    while not np.allclose(eigenvalues, eigenvalues_new, 0.000000001):
+    while not np.allclose(eigenvalues, eigenvalues_new, 1e-10):
         eigenvalues = eigenvalues_new
         Q, R = qr_matrix(matrix)
         matrix = np.dot(R, Q)
         eigenvalues_new = np.diag(matrix)
-        i += 1
-    print("Iterations: ", i)
     return eigenvalues_new
 
 
-print("Outcome: \n", matrix_eigenvalues(A))
+def gauss_jordan(matrix):
+    if matrix.shape[0] != matrix.shape[1]:
+        return "Matrix is not square"
+    matrix = matrix.copy()
+    n = matrix.shape[0]
+    epsilon = 1e-5
+    eigenvector = matrix_eigenvalues(matrix).tolist()
+    for i in range(n-1):
+        for j in range(i+1, n):
+            if np.abs(matrix[i][j]) < epsilon:
+                raise ZeroDivisionError
+            m = matrix[j][i] / matrix[i][i]
+            for k in range(i+1, n):
+                matrix[j][k] += m * matrix[i][k]
+
+    for i in range(n-1, -1, -1):
+        s = matrix[i][n-1]
+        for j in range(i, n, -1):
+            s -= matrix[i][j] * eigenvector[j]
+            if np.abs(matrix[i][i]) < epsilon:
+                raise ZeroDivisionError
+        eigenvector[i] = s / matrix[i][i]
+
+    return eigenvector
+
+
+def matrix_eigenvectors(matrix):
+    if matrix.shape[0] != matrix.shape[1]:
+        return "Matrix is not square"
+    matrix = matrix.copy()
+    eigenvalues = matrix_eigenvalues(matrix)
+    print(eigenvalues)
+    eigenvectors = []
+    for eigenvalue in eigenvalues:
+        eigen_matrix = matrix - np.diag(np.ones(matrix.shape[0])*eigenvalue)
+        eigenvectors.append(gauss_jordan(eigen_matrix))
+    return eigenvectors
+
+
+print(matrix_eigenvectors(A))
